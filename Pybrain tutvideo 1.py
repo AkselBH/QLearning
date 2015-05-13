7049ff7386326b5908af9641f54893a4ec5724a4
BaskerN <gusmouar@student.gu.se>
2015-05-13 15:07:44 +0200

    .

--- Pybrain tutvideo 1.py
+++ Pybrain tutvideo 1.py
@@ -6,16 +6,20 @@ from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers.backprop import BackpropTrainer
 
 D = SupervisedDataSet(2, 1)
+# 2 imput --> 1 output
 
 D.addSample([0,0], [0])
 D.addSample([0,1], [1])
 D.addSample([1,0], [1])
 D.addSample([1,1], [0])
+# 4 kombinationer av input och dess output "OR" funktion
 
 N = buildNetwork(2, 4, 1)
+# multilayer perception? med 1 gömt lager
 
 T = BackpropTrainer(N, learningrate = 0.01, momentum = 0.99)
+# momentum = reduced learningrate
 
-print ('MSE before'), T.testOnData(D)
+print (('MSE before'), T.testOnData(D))
 T.trainOnDataset(D, 1000)
-print ('MSE after'), T.testOnData(D)
+print (('MSE after'), T.testOnData(D))

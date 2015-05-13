7cae56ceae62e11d646f7920a7713dcbb08d60ad
BaskerN <gusmouar@student.gu.se>
2015-05-13 16:28:40 +0200

    .

---
+++ Maze.py
@@ -0,0 +1,41 @@
+__author__ = 'Stubborn'
+
+from numpy import *
+from matplotlib import pyplot as plt
+
+from pybrain.rl.environments.mazes import Maze, MDPMazeTask
+from pybrain.rl.agents import LearningAgent
+from pybrain.rl.learners import Q, ActionValueTable
+from pybrain.rl.experiments import Experiment
+
+envamatrix = array([[1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 0, 0, 0, 1, 0, 0, 0, 1],
+                    [1, 0, 1, 0, 1, 0, 1, 0, 1],
+                    [1, 0, 1, 0, 1, 0, 1, 0, 1],
+                    [1, 0, 1, 0, 1, 0, 1, 0, 1],
+                    [1, 0, 1, 0, 0, 0, 1, 0, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 0, 1],
+                    [1, 0, 0, 0, 0, 0, 0, 0, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1]])
+
+environment = Maze(envmatrix, (7, 7))
+
+task = MDPMaze Task (environment)
+
+table = ActionValueTable(81, 4)
+table.initialize(1.)
+
+agent = LearningAgent(table, Q())
+
+experiment = Experiment(task, agent)
+
+plt.ion()
+plt.gray()
+
+for i in range(1000):
+    eperiment.doInteractions(100=)
+    afent.learn()
+    agent.reset()
+
+    plt.pcolor(table.params.reshape(81,4).max(axis=1).reshape(9, 9))
+    plt.gcf().canvas.draw()

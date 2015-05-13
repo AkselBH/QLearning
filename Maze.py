fae6ad424e5ac8e4e17da9b41d957d998365fdc5
BaskerN <gusmouar@student.gu.se>
2015-05-13 16:46:44 +0200

    .

--- Maze.py
+++ Maze.py
@@ -3,12 +3,13 @@ __author__ = 'Stubborn'
 from numpy import *
 from matplotlib import pyplot as plt
 
-from pybrain.rl.environments.mazes import Maze, MDPMazeTask
+from pybrain.rl.environments.mazes import Maze
+from pybrain.rl.environments.mazes import MDPMazeTask
 from pybrain.rl.agents import LearningAgent
 from pybrain.rl.learners import Q, ActionValueTable
 from pybrain.rl.experiments import Experiment
 
-envamatrix = array([[1, 1, 1, 1, 1, 1, 1, 1, 1],
+envmatrix = array([[1, 1, 1, 1, 1, 1, 1, 1, 1],
                     [1, 0, 0, 0, 1, 0, 0, 0, 1],
                     [1, 0, 1, 0, 1, 0, 1, 0, 1],
                     [1, 0, 1, 0, 1, 0, 1, 0, 1],
@@ -20,7 +21,7 @@ envamatrix = array([[1, 1, 1, 1, 1, 1, 1, 1, 1],
 
 environment = Maze(envmatrix, (7, 7))
 
-task = MDPMaze Task (environment)
+task = MDPMazeTask(environment)
 
 table = ActionValueTable(81, 4)
 table.initialize(1.)
@@ -33,8 +34,8 @@ plt.ion()
 plt.gray()
 
 for i in range(1000):
-    eperiment.doInteractions(100=)
-    afent.learn()
+    experiment.doInteractions(100)
+    agent.learn()
     agent.reset()
 
     plt.pcolor(table.params.reshape(81,4).max(axis=1).reshape(9, 9))

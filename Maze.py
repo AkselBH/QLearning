171b72b569545173ab7021939a75378d96db69fd
BaskerN <gusmouar@student.gu.se>
2015-05-15 19:04:04 +0200

    Större array

--- Maze.py
+++ Maze.py
@@ -9,34 +9,46 @@ from pybrain.rl.agents import LearningAgent
 from pybrain.rl.learners import Q, ActionValueTable
 from pybrain.rl.experiments import Experiment
 
-envmatrix = array([[1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 0, 0, 0, 1, 0, 0, 0, 1],
-                    [1, 0, 1, 0, 1, 0, 1, 0, 1],
-                    [1, 0, 1, 0, 1, 0, 1, 0, 1],
-                    [1, 0, 1, 0, 1, 0, 1, 0, 1],
-                    [1, 0, 1, 0, 0, 0, 1, 0, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 0, 1],
-                    [1, 0, 0, 0, 0, 0, 0, 0, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1]])
+envmatrix = array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+                    [1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1],
+                    [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1],
+                    [1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1],
+                    [1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+                    [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1],
+                    [1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1],
+                    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
+                    [1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1],
+                    [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+                    [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+                    [1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+                    [1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
+                    [1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+                    [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
 
-environment = Maze(envmatrix, (7, 7))
+
+
+environment = Maze(envmatrix, (2, 2))
 
 task = MDPMazeTask(environment)
 
-table = ActionValueTable(81, 4)
+table = ActionValueTable(324, 4)
 table.initialize(1.)
 
 agent = LearningAgent(table, Q())
+#Tillämpar Q-algorithm (Q-learning) på .
 
 experiment = Experiment(task, agent)
 
 plt.ion()
-#plt.gray()
+plt.gray()
 
 for i in range(1000):
-    experiment.doInteractions(10)
+    experiment.doInteractions(100)
     agent.learn()
     agent.reset()
 
-    plt.pcolor(table.params.reshape(81,4).max(axis=1).reshape(9, 9))
+    plt.pcolor(table.params.reshape(324, 4).max(axis=1).reshape(18, 18))
     plt.gcf().canvas.draw()
